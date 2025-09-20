2d3da8c19bee2abb618825c56ccb8b3525a97a99
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-09-20 16:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,24 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
+
+	// select all prices
+	let prices = document.querySelectorAll(".price");
+
+	// calculate sum
+	let sum = 0;
+	prices.forEach((cell) => {
+		sum += Number(cell.textContent);
+	})
+	
+	// create the new row
+	let row = document.createElement("tr");
+	row.innerHTML = `<tr><td>Total</td><td>${sum}</td></tr>`;
+
+	// append the row to table
+	const table = document.querySelector("table");
+	table.appendChild(row);
+	
   
 };
 
